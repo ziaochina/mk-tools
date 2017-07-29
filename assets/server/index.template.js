7020a673c52cf63f4b76cc27d6f72cfda4622cc3
lsg <lsg@rrtimes.com>
2017-07-29 19:00:50 +0800

    更新server模板

--- assets/server/index.template.js
+++ assets/server/index.template.js
@@ -1,13 +1,11 @@
 const { config, start } = require("mk-server")
 const serverConfig = require( "./config")
 {{each services}}
-const {{$value.name}} = require("./{{$value.relaPath}}")
-{{/each}}
+const {{$value.name}} = require("./{{$value.relaPath}}"){{/each}}
 
 const services = {
-{{each services}}	
-    {{$value.name}},
-{{/each}}
+{{each services}}
+    {{$value.name}},{{/each}}
 }
 
 
