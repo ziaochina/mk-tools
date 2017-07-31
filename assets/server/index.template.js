fe7dae8cfec9f30c2229ed3a7bf7b3c571ad8be2
lisga <lsg@rrtimes.com>
2017-07-31 12:51:59 +0800

    server模板的代码格式

--- assets/server/index.template.js
+++ assets/server/index.template.js
@@ -1,14 +1,12 @@
 const { config, start } = require("mk-server")
-const serverConfig = require( "./config")
+const serverConfig = require("./config")
 {{each services}}
 const {{$value.name}} = require("./{{$value.relaPath}}"){{/each}}
 
-const services = {
-{{each services}}
+const services = { {{each services}}
     {{$value.name}},{{/each}}
 }
 
-
-config(serverConfig({services}))
+config(serverConfig({ services }))
 
 start()
