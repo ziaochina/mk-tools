2ac0938b583629d5bacda260b163ef4688abd76b
lishengguo <lishengguo@qq.com>
2017-08-02 18:20:32 +0800

    service模板修改config合并到index中

--- assets/service/template/api.js
+++ assets/service/template/api.js
@@ -1,8 +1,30 @@
-const config = require("./config").current
+/*
+* api.js        //webapi接口定义（接口参数和返回值的格式转换）
+*/
+var config
+const api = {
 
-const ping = (dto) => true
+    _init: (current) => config = current,
 
-module.exports = {
-    ping,
+    ping: (data, ctx) => true,
 }
+module.exports = api
+
+
+
+/*
+* service.js    //业务逻辑
+*/
+
+
+
+/*
+* dao.js        //数据库处理
+*/
+
+
+
+/*
+* model.js      //数据模型定义
+*/
 
--- assets/service/template/index.js
+++ assets/service/template/index.js
@@ -1,7 +1,9 @@
-const config = require("./config")
+/*
+ *  index.js        //属性说明，导出的config方法在服务器启动时调用，api对象的下级方法绑定到对应的url，如: api.create 方法绑定的url是 "/${name}/create"
+*/
 const api = require("./api")
 
-module.exports = {
+const index = {
     name: "${serviceName}",
     version: "",
     description: "",
@@ -9,3 +11,19 @@ module.exports = {
     config,
     api,
 }
+
+module.exports = index;
+
+
+
+ 
+/*
+ *  config.js        //初始化参数设置
+*/
+
+function config(options) {
+    var current = config.current
+    Object.assign(current, options)
+    api._init(current)
+} 
+config.current = {}
