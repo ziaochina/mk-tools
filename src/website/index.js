e84d72e70498301ae58b9dc0a7fb67d8dd15f951
ziaochina <ziaochina@gmail.com>
2017-09-27 16:55:41 +0800

    zlj

--- src/website/index.js
+++ src/website/index.js
@@ -42,8 +42,8 @@ function createWebsite(websiteName, dest) {
 
 			await runCmd(which.sync(npm), [
 				'install',
-				'react@15.6.1',
-				'react-dom@15.6.1',
+				'react',
+				'react-dom',
 				'mk-meta-engine',
 				'mk-component',
 				'mk-utils',
