d4ebfbb5ed0c2318849aa7a5572aa325c414bd05
ziaochina <ziaochina@gmail.com>
2017-10-13 10:31:43 +0800

    zlj

--- assets/website/template/webpack.config.js
+++ assets/website/template/webpack.config.js
@@ -75,6 +75,7 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     name: '[name].[hash:8].[ext]',
+                    limit: 8192
                 }
             }
         }],
